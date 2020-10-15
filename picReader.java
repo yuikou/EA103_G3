0f99d7aad2e902a02356c6281d533b631df23b79
ea103T41 <ea103T41@gmail.com>
2020-10-15 16:10:00 +0800

    picReader Integration Start

--- picReader.java
+++ picReader.java
@@ -1,4 +1,3 @@
-package com.sitLic.controller;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -8,27 +7,28 @@ import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.member.model.MemService;
+import com.member.model.MemVO;
 import com.sitLic.model.SitLicService;
 import com.sitLic.model.SitLicVO;
 
+@WebServlet("/picReader")
+public class picReader extends HttpServlet {
+	private static final long serialVersionUID = 8772772431614292284L;
 
-public class ShowImg extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		doPost(req, res);
 	}
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		// 處理中文請求
-		req.setCharacterEncoding("UTF-8");
 		String action = req.getParameter("action");
 		
-		/* 來自sitLic的請求 - 顯示圖片 */
+		/* ----------于婷---------- */
 		if ("sitLic".equals(action)) {
 			List<String> errorMsgs = new LinkedList<String>();
 			req.setAttribute("errorMsgs", errorMsgs);
@@ -52,6 +52,7 @@ public class ShowImg extends HttpServlet {
 				int len;
 				while ((len = bin.read(buffer)) != -1) {
 					out.write(buffer, 0 , len);
+					out.flush();
 				}
 				bin.close();
 						
@@ -60,6 +61,40 @@ public class ShowImg extends HttpServlet {
 			} catch (Exception e) {
 				errorMsgs.add("無法取得資料:" + e.getMessage());
 				RequestDispatcher failureView = req.getRequestDispatcher("/sitLic/showOneSitLic.jsp");
+				failureView.forward(req, res);
+			}
+					
+		}
+		
+		if ("sitFollow".equals(action)) {
+			List<String> errorMsgs = new LinkedList<String>();
+			req.setAttribute("errorMsgs", errorMsgs);
+					
+			try {
+				/*************************** 1.接收請求參數 ****************************************/
+				String memNo = req.getParameter("memNo");
+						
+				/***************************2.開始查詢資料*****************************************/
+				MemService slSvc = new MemService();
+				MemVO memVo = slSvc.getOneMem(memNo);
+				
+				res.setContentType("image/jpg");
+				ServletOutputStream out = res.getOutputStream();
+						
+				ByteArrayInputStream bin = new ByteArrayInputStream(memVo.getMemPhoto());
+						
+				byte[] buffer = new byte[4*1024];
+				int len;
+				while ((len = bin.read(buffer)) != -1) {
+					out.write(buffer, 0 , len);
+				}
+				bin.close();
+						
+					
+			/***************************其他可能的錯誤處理*************************************/
+			} catch (Exception e) {
+				errorMsgs.add("無法取得資料:" + e.getMessage());
+				RequestDispatcher failureView = req.getRequestDispatcher("/sitFollow/listSitFollow.jsp");
 				failureView.forward(req, res);
 			}
 					
